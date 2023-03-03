83a608b26eb445a7834e386b77b89b4f597c6b59
João Batista Santos Junior <joao.santos@programmers.com.br>
2023-03-03 14:54:42 -0300

    Add seniority service.

--- src/Application/Services/Interfaces/ISeniorityService.cs
+++ src/Application/Services/Interfaces/ISeniorityService.cs
@@ -1,6 +1,12 @@
-﻿
+﻿using Application.DTOs;
+
 namespace Application.Services.Interfaces;
 
 public interface ISeniorityService
 {
+    Task<ICollection<SeniorityDto>> GetSeniorityAsync();
+    Task<SeniorityDto> GetByIdAsync(int id);
+    Task<SeniorityDto> CreateAsync(SeniorityDto seniorityDto);
+    Task UpdateAsync(SeniorityDto seniorityDto);
+    Task DeleteAsync(int id);
 }
--- src/Application/Services/Service/SeniorityService.cs
+++ src/Application/Services/Service/SeniorityService.cs
@@ -1,7 +1,57 @@
-﻿using Application.Services.Interfaces;
+﻿using Application.DTOs;
+using Application.Services.Interfaces;
+using AutoMapper;
+using Domain.Interfaces.Repositories;
+using Domain.Models;
 
 namespace Application.Services.Service;
 
 public class SeniorityService : ISeniorityService
 {
+    private readonly ISeniorityRepository _repository;
+    private readonly IMapper _mapper;
+
+    public SeniorityService(ISeniorityRepository repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    public async Task<SeniorityDto> CreateAsync(SeniorityDto seniorityDto)
+    {
+        var seniority = _mapper.Map<Seniority>(seniorityDto);
+
+        await _repository.CreateAsync(seniority);
+
+        seniorityDto = _mapper.Map<SeniorityDto>(seniority);
+
+        return seniorityDto;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _repository.DeleteAsync(id);
+    }
+
+    public async Task<SeniorityDto> GetByIdAsync(int id)
+    {
+        var seniorityBd = await _repository.GetByIdAsync(id);
+        var seniority = _mapper.Map<SeniorityDto>(seniorityBd);
+
+        return seniority;
+    }
+
+    public async Task<ICollection<SeniorityDto>> GetSeniorityAsync()
+    {
+        var senioritiesDb = await _repository.GetSeniorityAsync();
+        var seniorities = _mapper.Map<ICollection<SeniorityDto>>(senioritiesDb);
+
+        return seniorities;
+    }
+
+    public async Task UpdateAsync(SeniorityDto seniorityDto)
+    {
+        var seniority = _mapper.Map<Seniority>(seniorityDto);
+        await _repository.UpdateAsync(seniority);
+    }
 }
